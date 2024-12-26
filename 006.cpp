53ba762b2567e172f9d2d2379900de9afb9e2c38
Arya <aryanoble06@gmail.com>
2024-12-26 19:42:36 +0530

    Day 2

---
+++ 006.cpp
@@ -0,0 +1,20 @@
+// Program to find sum of digits.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // to find the sum of digits
+    int n;
+    cout << "Enter a number : ";
+    cin >> n;
+    int temp = 0;
+    while (n != 0)
+    {
+        temp += (n % 10);
+        n = n / 10;
+    }
+    cout << "Sum of the digits of the given number : " << temp << endl;
+    return 0;
+}
