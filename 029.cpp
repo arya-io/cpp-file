90ad728d0aad5c103a7c985bff958a891123df8e
Arya <aryanoble06@gmail.com>
2024-12-31 18:22:20 +0530

    Day 7

---
+++ 029.cpp
@@ -0,0 +1,19 @@
+// Program to generate random number.
+
+#include <iostream>
+#include <stdlib.h> //for rand()
+#include <time.h>
+
+int main()
+{
+    // to print n random numbers
+    int n;
+    std::cout << "Enter n : ";
+    std::cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        // srand(time(0));
+        std::cout << rand() << std::endl;
+    }
+}
